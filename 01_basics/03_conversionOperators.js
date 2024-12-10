44a71646aad35838409d0268e678b08ca18a8cb4
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-10 15:20:33 +0000

    operators

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -15,17 +15,52 @@ let valueInNumber = Number(score)
 let isLoggedIn =  "dlf"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "sdhf" => true
 
 
-let someNum = 44
+// let someNum = 44
 
-let stringNum = String(someNum)
-console.log(stringNum)
-console.log(typeof stringNum)
+// let stringNum = String(someNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+// **************Operators****************
 
 
+let value = 3
+let negative = -value
+// console.log(negative)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " sai"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1+ "2")
+// console.log("1" + 2 + 2  )
+// console.log(1 + 2 +"2")
+
+// console.log(+"")
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);// 101 
+
+// prefix postfix : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
