008672795316ce6e577fbdef62db30a95c04ad99
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-10 14:41:10 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3 + 5 ) // we are using node js not browser
+
+console.log(2
+    +3) // coee readability should be high
+console.log("saiteja")
+
+
+let name ="saiteja"
+let age = 18
+let state;
+
+// number => 2 to power 53 
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but not initialized
+// symbol => unique 
+
+
+// Object 
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
