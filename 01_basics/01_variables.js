10a4011fb728b11f05072e048cbffe32a9223915
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-10 13:15:52 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "g@google.com"
+var accountPassword = "12345"
+accountCity = "madras"
+
+// accountId = 2 // not allowed 
+
+
+
+
+accountEmail = "hai@gmail.com"
+accountPassword = "54321"
+accountCity = "hyderabad"
+let accountState;
+console.log(accountId)
+/*
+prefer not to use war 
+because of issue in block scope and functional scope
+
+*/
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
