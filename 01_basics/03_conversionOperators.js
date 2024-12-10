f727f302227d23b9be0288fc508b9bf9d02cd1b1
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-10 14:57:16 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,31 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33 " => 33
+// "30abc" => NaN 
+// true => 1; false => 0
+
+let isLoggedIn =  "dlf"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sdhf" => true
+
+
+let someNum = 44
+
+let stringNum = String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
+
+
