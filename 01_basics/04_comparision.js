e566d235006f6d86dcc4cfe93823c33a148526a8
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-11 02:06:31 +0000

    comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,1 +1,23 @@
-console.log(2>4);
+// console.log(2>4)
+// console.log(2>=1)
+// console.log(2<=2)
+// console.log(2<=4)
+// console.log(2==4)
+// console.log(2!=4)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null != 0)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === stirctly check 
+console.log("2" === 2);
