48af5ad6f9f438c12718c6a23ad56346552c4b39
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-11 09:00:56 +0000

    stack and heap(reference)

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,7 +11,7 @@ let userEmail;
 const id = Symbol('123')
 const id2 = Symbol('123')
 
-console.log(id === id2)
+// console.log(id === id2)
 
 const bigNum = 3444444444444444322n
 
@@ -29,13 +29,38 @@ let obj={
 
 }
 
-const my = function() {
-    console.log("hello world")
-}
+// const my = function() {
+//     console.log("hello world")
+// }
 
 
-console.log(typeof outsideTemp);
-console.log(typeof my)
+// console.log(typeof outsideTemp);
+// console.log(typeof my)
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
+
+
+//   ********************
+
+// stack (primitive) , heap (non-primitive)
+
+let myyt = "sai"
+let otheryt =myyt
+
+otheryt = "bye"
+console.log(otheryt)
+console.log(myyt)
+
+let userOne = {
+    email : "com@gmail.com",
+    pass : 123457
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "userTwo@gmail.com"
+
+console.log(userOne.email) // userTwo@gmail.com
+console.log(userTwo.email) // same as above because it reference
