76b961ba538039a8712149321c0f4f617bcdca43
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-12 05:00:13 +0000

    date and time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,17 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("01-02-2003") // yymmdd latest dd-mm-yyy
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime());
+console.log(Date.now())
+
+
