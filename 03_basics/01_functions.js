94f5348b7ed5120f7d9ecd332889d9053ec463f8
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-13 16:36:16 +0000

    functions calling with obj and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -44,4 +44,32 @@ function loginUserMessage(username = "sai") {
 
 
 // console.log(loginUserMessage("saiteja"))
-console.log(loginUserMessage("teja"))
+// console.log(loginUserMessage("teja"))
+
+function calculateCartPrice(val1,val2,...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(4,3,4,2))
+
+// const user = {
+//     username: "sai",
+//     price: 100
+// }
+
+function us(obj) {
+    console.log(`username is ${obj.username} and price is ${obj.price}`)
+}
+
+// us(user) // we  mention obj name in it 
+us({username: "hai",price: 233}) // we can directly pass obj also 
+
+const myArray = [100, 300, 400, 500]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myArray))
+console.log(returnSecondValue([100, 200, 300, 400]))
+
