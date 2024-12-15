4f71e33b1f4cbf4590f44fa12c9b4cc729917fd3
alamurisaiteja <142009932+alamurisaiteja@users.noreply.github.com>
2024-12-15 11:25:58 +0000

    while , do

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(`value of index is ${i}`);
+//     i = i + 2
+// }
+
+let myArray = ['flash','batman', 'superman']
+let i1 = 0
+while (i1<myArray.length) {
+    console.log(`value is ${myArray[i1]}`)
+    i1 ++
+}
+
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
+
+// atleast execute one time even if condition is false 
