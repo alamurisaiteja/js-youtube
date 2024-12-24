c7216b70a6d161f7d7665b809ef1b95c759fa67c
alamurisaiteja <alamurisaiteja@gmail.com>
2024-12-24 19:26:54 +0530

    oops.js modified

--- 10_class_oops/oop.js
+++ 10_class_oops/oop.js
@@ -1,8 +1,44 @@
 const user = {
     username :'sai',
     logincount : 8,
-    signedIn : true
+    signedIn : true,
+
+    getuserdetails : function() {
+        // console.log("got user details from database")
+        console.log(`username${this.username}`);
+        console.log(this)
+        
+    }
 }
 
 
-console.log(user.signedIn)
+
+// console.log(user.signedIn)
+// // console.log(user.getuserdetails())
+// console.log(this)
+
+// new is constructure function
+// const promiseOne = new Promise()
+// const date = new Date()
+
+
+
+function user1(username, logincount, isLoggedIn) {
+    this.username = username;
+    this.logincount = logincount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this
+}
+// here if we dont use new key word (constructor) (means it will create new instance) then the values will be overrided and becomes the latest value it would be a mess
+const userOne = new user1('sai', 3, true)
+const userTwo = new user1('chaiaurcode', 11, false)
+
+console.log(userOne);
+console.log(userTwo);
+
+
+// new creates empty object called instances
+// step2 : it will call constructor function because new
+// step3 : what we have arguments will be injected into this
+// step4 : directly comes inside fun
